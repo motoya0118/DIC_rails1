d40893c4760c4a20a604958fc7a2c1168144ee4b
motoya <motoyao0118@gmail.com>
2022-06-29 15:05:18 +0900

    2nd commit

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,10 @@
 class Contact < ApplicationRecord
-  validates :name, presence: true
-  validates :Email, presence: true
-  validates :content, presence: true
-  validates :name, length: {in: 1..140}
-  validates :Email, length: {in: 1..140}
-  validates :content, length: {in: 1..140}
-
+  validates :name,:Email,:content, presence: true ,length: {in: 1..140}
 end
+
+#validates :name, presence: true
+#validates :Email, presence: true
+#validates :content, presence: true
+#validates :name, length: {in: 1..140}
+#validates :Email, length: {in: 1..140}
+#validates :content, length: {in: 1..140}
